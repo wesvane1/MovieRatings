a161c1d41e1737d4f754b7e2cf16abbddd5e18f2
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2025-03-05 08:55:49 -0700

    feat: movie edit now works!

--- src/app/movies/movies-edit/movies-edit.component.ts
+++ src/app/movies/movies-edit/movies-edit.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { Movie } from '../movies.model';
+import { MoviesService } from '../movies.service';
+import { Review } from '../../reviews/review.model';
 
 @Component({
   selector: 'app-movies-edit',
@@ -6,6 +11,61 @@ import { Component } from '@angular/core';
   templateUrl: './movies-edit.component.html',
   styleUrl: './movies-edit.component.scss'
 })
-export class MoviesEditComponent {
+export class MoviesEditComponent implements OnInit{
+
+  originalMovie: Movie;
+  movie: Movie;
+  editMode: boolean = false;
+  id: string;
+
+  constructor(
+    private moviesService: MoviesService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ){}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      const id = params['id']
+      if (!id){
+        this.editMode = false
+        return;
+      }
+      this.originalMovie = this.moviesService.getMovie(id)
+
+      if (!this.originalMovie){
+        return
+      }
+      this.editMode = true;
+      this.movie = {...this.originalMovie}
+    })
+  }
+
+  onSubmit(form: NgForm){
+    const value = form.value
+    let movie = new Movie(
+      value.id,
+      value.title,
+      value.description,
+      null
+    )
+    if(this.editMode){
+      this.moviesService.updateMovie(this.originalMovie, movie)
+      this.onUpdate()
+    } else{
+      this.moviesService.addMovie(movie)
+      this.onCreate()
+    }
+  }
+
+  onCreate(){
+    this.router.navigate(['/movie'])
+  }
+  onUpdate(){
+    this.router.navigate(['/movie'])
+  }
+  onCancel(){
+    this.router.navigate(['/movie'])
+  }
 
 }

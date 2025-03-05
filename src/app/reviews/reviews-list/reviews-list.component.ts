986fb24f05b5b4bd439a184e9c9160731e84af2b
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2025-03-05 09:35:36 -0700

    feat: review list works

--- src/app/reviews/reviews-list/reviews-list.component.ts
+++ src/app/reviews/reviews-list/reviews-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MoviesService } from '../../movies/movies.service';
+import { Movie } from '../../movies/movies.model';
 
 @Component({
   selector: 'app-reviews-list',
@@ -7,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './reviews-list.component.scss'
 })
 export class ReviewsListComponent {
+
+    movieListReview: Movie[] = []
+    term: string = '';
+  
+    constructor(
+      private readonly moviesService: MoviesService,
+    ){}
+  
+    ngOnInit(){
+      this.movieListReview = this.moviesService.getMovies()
+    }
+  
+    search(value: string) {
+      this.term = value;
+    }
 
 }

ffd858e29f256da2dbdbe772aab8d32f651d0c6f
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2025-03-05 10:04:55 -0700

    chore: changes to review detail

--- src/app/reviews/reviews-detail/reviews-detail.component.ts
+++ src/app/reviews/reviews-detail/reviews-detail.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Movie } from '../../movies/movies.model';
+import { MoviesService } from '../../movies/movies.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ReviewsService } from '../reviews.service';
+import { Review } from '../review.model';
 
 @Component({
   selector: 'app-reviews-detail',
@@ -6,6 +11,26 @@ import { Component } from '@angular/core';
   templateUrl: './reviews-detail.component.html',
   styleUrl: './reviews-detail.component.scss'
 })
-export class ReviewsDetailComponent {
+export class ReviewsDetailComponent implements OnInit{
+  review: Review;
 
+  constructor(
+    private readonly moviesService: MoviesService,
+    private readonly reviewService: ReviewsService,
+    private readonly router: Router,
+    private readonly route: ActivatedRoute,
+  ){}
+
+  ngOnInit(){
+    this.route.params.subscribe((params: Params) => {
+      const movieId = params['movieId'];
+      this.review = this.reviewService.getReviewsForMovie(movieId) || {} as Review;
+    });
+    
+  }
+
+  onDelete() {
+    // this.moviesService.deleteMovie(this.review);
+    this.router.navigate(['/review'])
+ }
 }

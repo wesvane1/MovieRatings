1ba3fccf9683b7c951e3111df6c7f1831f5d03e0
wesvane1 <113637824+wesvane1@users.noreply.github.com>
2025-03-05 09:35:50 -0700

    feat: review item populates

---
+++ src/app/reviews/reviews-item/reviews-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Movie } from '../../movies/movies.model';
+
+@Component({
+  selector: 'app-reviews-item',
+  standalone: false,
+  templateUrl: './reviews-item.component.html',
+  styleUrl: './reviews-item.component.scss'
+})
+export class ReviewsItemComponent {
+
+  @Input() movieReview: Movie;
+
+}
